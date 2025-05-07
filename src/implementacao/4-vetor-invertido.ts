23ee82c70a970b0f8c71c3ce0bdbd2542c53a722
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-07 13:10:58 -0300

    Resolução exercício 4.

--- src/implementacao/4-vetor-invertido.ts
+++ src/implementacao/4-vetor-invertido.ts
@@ -1,3 +1,13 @@
-function inverterVetor() {
-    return null;
+function inverterVetor(vetorOriginal: number []): number [] {
+    const vetorInvertido: number [] = [];
+
+    for (let i = 0;i < vetorOriginal.length; i++) {
+        vetorInvertido[i] = vetorOriginal[vetorOriginal.length - (i+1)]
+        console.log("indice: " + i)
+        console.log("indice final: " + (vetorOriginal.length - (i+1)))
+    }
+    
+        return vetorInvertido;
 }
+const input4 = [1, 2, 3, 4, 5, 6];
+console.log(inverterVetor(input4));
