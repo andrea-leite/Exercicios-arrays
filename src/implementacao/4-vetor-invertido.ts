d3628915de6853d0f87b16483d8a0727c06705f3
dronreef2 <202001579176@alunos.estacio.br>
2025-05-08 11:32:14 -0300

    exercicios 4 correcao, 5implemetacao

--- src/implementacao/4-vetor-invertido.ts
+++ src/implementacao/4-vetor-invertido.ts
@@ -3,11 +3,10 @@ function inverterVetor(vetorOriginal: number []): number [] {
 
     for (let i = 0;i < vetorOriginal.length; i++) {
         vetorInvertido[i] = vetorOriginal[vetorOriginal.length - (i+1)]
-        console.log("indice: " + i)
-        console.log("indice final: " + (vetorOriginal.length - (i+1)))
     }
     
         return vetorInvertido;
 }
+
 const input4 = [1, 2, 3, 4, 5, 6];
 console.log(inverterVetor(input4));
--- src/implementacao/5-notas-faixa.ts
+++ src/implementacao/5-notas-faixa.ts
@@ -1,3 +1,24 @@
-function contarNotasNaFaixa() {
-    return null;
+function contarNotasNaFaixa(notas: number[], media:number):string {
+    const valorAcimaMedia: number  = media *1.1;
+    const valorAbaixoMedia: number  = media *0.9;
+    let contadorDeNotas = 0; 
+
+    for (let i= 0; i < notas.length; i++){
+        if(notas[i] <= valorAcimaMedia && notas[i] >= valorAbaixoMedia){
+            contadorDeNotas++; 
+        }
+    } 
+
+    return `${contadorDeNotas} estão entre ${valorAbaixoMedia.toFixed(1)} e ${valorAcimaMedia.toFixed(1)}`;
 }
+
+const notas: number[] = [
+    7.5, 8.0, 6.5, 9.2, 5.8, 4.0, 10.0, 6.9, 8.3, 7.0,
+    9.5, 3.6, 5.0, 8.7, 6.0, 4.5, 7.8, 6.4, 2.0, 5.2,
+    9.0, 7.2, 8.1, 3.0, 6.6, 7.3, 9.8, 4.9, 5.5, 6.8,
+    8.9, 7.7, 3.5, 6.1, 5.6, 2.5, 9.9, 6.2, 7.1, 4.3,
+    5.7, 8.6, 9.1, 6.3, 4.1, 3.9, 7.6, 5.4, 2.8, 4.7
+];
+
+const media = 6;
+console.log(contarNotasNaFaixa(notas,media));
